55a205c3f8adb1d8577c545840984952b0669986
wangfeng <wangfeng211731@sogou-inc.com>
2017-09-26 18:45:27 +0800

    update

--- crawler.go
+++ crawler.go
@@ -3,58 +3,94 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
 	"sync"
 )
 
 var wg sync.WaitGroup
 
+const ADMIN_USER_ID = "QGVbe7gWfyLfb046Xntz0Q__"
+
 //消费者线程函数
-func consume(urls chan string) {
+func consume(scids chan string) {
 	defer wg.Done()
-	f, err := os.OpenFile("./test", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	for url := range urls {
-		if _, err = f.WriteString(url + "\n"); err != nil {
-			panic(err)
-		}
-
+	for scid := range scids {
+		url := "http://api.miaopai.com/m/v2_channel.json?fillType=259&scid=" + scid
+		res := getBody(url)
+		fmt.Println(res)
 	}
 
 }
 
 //生产者线程函数
-func produce(c chan string) {
+func produce(uids chan string, scids chan string) {
 	defer wg.Done()
-	for i := 0; i < 20; i++ {
-		url := strconv.Itoa(i)
-		c <- url
+	for uid := range uids {
+		url := "http://www.miaopai.com/gu/u?fen_type=channel&suid=" + uid
+		res := getBody(url)
+		match := regexp.MustCompile(`data-scid=\\"([\S]+)\\"`)
+		for _, scid := range match.FindAllString(res, -1) {
+			temps := strings.Split(scid, "\"")
+			scid = strings.Trim(temps[1], "\\")
+			fmt.Println(scid)
+			scids <- scid
+		}
 	}
-	close(c)
+	close(scids)
+}
+
+func getBody(url string) string {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	} else {
+		defer response.Body.Close()
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println(err)
+			return ""
+		}
+		res := string(body)
+		return res
+	}
 }
 
 func main() {
-	c := make(chan string)
+	scids := make(chan string)
 	//消费者数量
-	n := 5
+	n := 10
 	//生产者数量
 	m := 1
 	wg.Add(n + m)
 
-	for i := 0; i < n; i++ {
-		go consume(c)
-	}
-
+	uids := make(chan string)
 	for i := 0; i < m; i++ {
-		go produce(c)
+		go produce(uids, scids)
 	}
 
-	//这里可以执行reduce操作，比如合并多个线程的结果
+	for i := 0; i < n; i++ {
+		go consume(scids)
+	}
+
+	res := getBody("http://www.miaopai.com/gu/follow?suid=" + ADMIN_USER_ID)
+	if res == "" {
+		return
+	}
+	match := regexp.MustCompile(`suid=\\"([\S]+)\\"`)
+	for _, uid := range match.FindAllString(res, -1) {
+		temps := strings.Split(uid, "\"")
+		uid = strings.Trim(temps[1], "\\")
+		fmt.Println(uid)
+		uids <- uid
+	}
+
+	close(uids)
 
 	wg.Wait()
+
+	fmt.Println("done")
 }

47115ff7f626ef22307a437de3d69b4adde2dc79
wangfeng <wangfeng211731@sogou-inc.com>
2018-01-24 16:20:30 +0800

    判断两棵二叉树是否相等

--- equivalent_binary_trees.go
+++ equivalent_binary_trees.go
@@ -1,53 +1,84 @@
 package main
 
-import "golang.org/x/tour/tree"
-import "fmt"
-import "sort"
+import (
+	"fmt"
+	"sort"
 
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
+	"golang.org/x/tour/tree"
+)
+
 func Walk(t *tree.Tree, ch chan int) {
 	if t == nil {
+		close(ch)
 		return
 	}
-	Walk(t.Left, ch)
-	ch <- t.Value
-	Walk(t.Right, ch)
+	queue := make([]*tree.Tree, 0)
+	queue = append(queue, t)
+
+	for len(queue) != 0 {
+		node := queue[0]
+		queue = queue[1:]
+		ch <- node.Value
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
+
+	close(ch)
+
 }
 
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int, 10)
-	go Walk(t1, ch1)
-	ch2 := make(chan int, 10)
-	go Walk(t2, ch2)
-
-	s1 := make(sort.IntSlice, 10)
-	for i := 0; i < 10; i++ {
-		s1[i] = <-ch1
+func equal(s1 []int, s2 []int) bool {
+	if s1 == nil && s2 == nil {
+		return true
 	}
-	close(ch1)
-
-	s2 := make(sort.IntSlice, 10)
-	for i := 0; i < 10; i++ {
-		s2[i] = <-ch2
+	if s1 == nil || s2 == nil {
+		return false
 	}
-	close(ch2)
-
-	s1.Sort()
-	s2.Sort()
-
-	for i := 0; i < 10; i++ {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
 		if s1[i] != s2[i] {
 			return false
 		}
 	}
-
 	return true
 }
 
+func Same(t1, t2 *tree.Tree) bool {
+	if t1 == nil && t2 == nil {
+		return true
+	}
+	if t1 == nil || t2 == nil {
+		return false
+	}
+
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	s1 := make([]int, 10)
+	s2 := make([]int, 10)
+	for item1 := range ch1 {
+		s1 = append(s1, item1)
+	}
+	for item1 := range ch2 {
+		s2 = append(s2, item1)
+	}
+
+	sort.Ints(s1)
+	sort.Ints(s2)
+	return equal(s1, s2)
+}
+
 func main() {
-	res := Same(tree.New(1), tree.New(1))
-	fmt.Println(res)
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+	fmt.Println(Same(nil, tree.New(2)))
+	fmt.Println(Same(nil, nil))
 }

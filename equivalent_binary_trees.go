5597c44ad2df04955d3005bab986b98aea9ccf39
wangfeng <wangfeng211731@sogou-inc.com>
2018-01-30 15:29:43 +0800

    update

--- equivalent_binary_trees.go
+++ equivalent_binary_trees.go
@@ -2,51 +2,21 @@ package main
 
 import (
 	"fmt"
-	"sort"
 
 	"golang.org/x/tour/tree"
 )
 
-func Walk(t *tree.Tree, ch chan int) {
-	if t == nil {
-		close(ch)
-		return
+func walk(t *tree.Tree, ch chan int) {
+	if t != nil {
+		walk(t.Left, ch)
+		ch <- t.Value
+		walk(t.Right, ch)
 	}
-	queue := make([]*tree.Tree, 0)
-	queue = append(queue, t)
-
-	for len(queue) != 0 {
-		node := queue[0]
-		queue = queue[1:]
-		ch <- node.Value
-		if node.Left != nil {
-			queue = append(queue, node.Left)
-		}
-		if node.Right != nil {
-			queue = append(queue, node.Right)
-		}
-	}
-
-	close(ch)
-
 }
 
-func equal(s1 []int, s2 []int) bool {
-	if s1 == nil && s2 == nil {
-		return true
-	}
-	if s1 == nil || s2 == nil {
-		return false
-	}
-	if len(s1) != len(s2) {
-		return false
-	}
-	for i := range s1 {
-		if s1[i] != s2[i] {
-			return false
-		}
-	}
-	return true
+func Walk(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
 }
 
 func Same(t1, t2 *tree.Tree) bool {
@@ -62,18 +32,17 @@ func Same(t1, t2 *tree.Tree) bool {
 	go Walk(t1, ch1)
 	go Walk(t2, ch2)
 
-	s1 := make([]int, 10)
-	s2 := make([]int, 10)
-	for item1 := range ch1 {
-		s1 = append(s1, item1)
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		if v1 != v2 || ok1 != ok2 {
+			return false
+		}
+		if !ok1 {
+			break
+		}
 	}
-	for item1 := range ch2 {
-		s2 = append(s2, item1)
-	}
-
-	sort.Ints(s1)
-	sort.Ints(s2)
-	return equal(s1, s2)
+	return true
 }
 
 func main() {
